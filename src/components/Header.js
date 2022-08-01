821dbd8fe51e0de0ed55d79ee3c815cba6821dac
hafizbali315 <hafizbali315@gmail.com>
2022-08-02 04:06:05 +0500

    Header and Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,50 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+
+import HeaderSectionBg from '../assets/images/HeaderBg1.png'
+
 const Header = () => {
-	return <div className="header">Header</div>
+	const [countValue, setCountValue] = useState(4)
+
+	const increment = () => {
+		if (countValue >= 0 && countValue < 9) {
+			setCountValue(countValue + 1)
+		}
+	}
+
+	const decrement = () => {
+		if (countValue <= 9 && countValue > 0) {
+			setCountValue(countValue - 1)
+		}
+	}
+
+	return (
+		<div className="header">
+			<Navbar />
+
+			<div className="content_section">
+				<h1>SEPPUKU</h1>
+
+				<div className="counter">
+					<button className="count" onClick={decrement}>
+						{' '}
+						-{' '}
+					</button>
+					<div className="input_container">{countValue}</div>
+					<button className="count" onClick={increment}>
+						{' '}
+						+{' '}
+					</button>
+				</div>
+
+				<button className="mint_btn">mint</button>
+			</div>
+
+			<div className="bg">
+				<img src={HeaderSectionBg} alt="headerbg" />
+			</div>
+		</div>
+	)
 }
 
 export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,137 @@
+import { useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+
+import EtherscanIcon from '../assets/images/EtherscanIcon.png'
+import OpenseaIcon from '../assets/images/OpenseaIcon.png'
+import TwitterIcon from '../assets/images/TwitterIcon.png'
+import DiscordIcon from '../assets/images/DiscordIcon.png'
+
 const Navbar = () => {
-	return <div className="navbar">Navbar</div>
+	const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+	return (
+		<div className="navbar">
+			<ul className="links">
+				<li>
+					<a className="link" href="#home">
+						Home
+					</a>
+				</li>
+				<li>
+					<a className="link" href="#honor">
+						Honor
+					</a>
+				</li>
+				<li>
+					<a className="link" href="#lore">
+						Lore
+					</a>
+				</li>
+				<li>
+					<a className="link" href="#team">
+						team
+					</a>
+				</li>
+				<li>
+					<a className="link" href="#faq">
+						faq
+					</a>
+				</li>
+			</ul>
+
+			<ul className="social_links">
+				<li>
+					<a className="link" href="https://etherscan.io/">
+						<img src={EtherscanIcon} alt="EtherscanIcon" />
+					</a>
+				</li>
+				<li>
+					<a className="link" href="https://opensea.io/">
+						<img src={OpenseaIcon} alt="OpenseaIcon" />
+					</a>
+				</li>
+				<li>
+					<a className="link" href="https://twitter.com/">
+						<img src={TwitterIcon} alt="TwitterIcon" />
+					</a>
+				</li>
+				<li>
+					<a className="link" href="https://discord.com/">
+						<img src={DiscordIcon} alt="DiscordIcon" />
+					</a>
+				</li>
+			</ul>
+
+			<div className="mobile_navbar">
+				<div className="logo">SEPPUKU</div>
+				<div className="hamburger">
+					{showMobileMenu ? (
+						<span onClick={() => setShowMobileMenu(false)} className="icon">
+							{' '}
+							X{' '}
+						</span>
+					) : (
+						<GiHamburgerMenu onClick={() => setShowMobileMenu(true)} className="icon" />
+					)}
+				</div>
+			</div>
+
+			{showMobileMenu && (
+				<ul className="mobile_menu">
+					<li>
+						<a className="link" onClick={() => setShowMobileMenu(false)} href="#home">
+							Home
+						</a>
+					</li>
+					<li>
+						<a className="link" onClick={() => setShowMobileMenu(false)} href="#honor">
+							Honor
+						</a>
+					</li>
+					<li>
+						<a className="link" onClick={() => setShowMobileMenu(false)} href="#lore">
+							Lore
+						</a>
+					</li>
+					<li>
+						<a className="link" onClick={() => setShowMobileMenu(false)} href="#team">
+							team
+						</a>
+					</li>
+					<li>
+						<a className="link" onClick={() => setShowMobileMenu(false)} href="#faq">
+							faq
+						</a>
+					</li>
+
+					<li>
+						<ul className="social_links">
+							<li>
+								<a className="link" onClick={() => setShowMobileMenu(false)} href="https://etherscan.io/">
+									<img src={EtherscanIcon} alt="EtherscanIcon" />
+								</a>
+							</li>
+							<li>
+								<a className="link" onClick={() => setShowMobileMenu(false)} href="https://opensea.io/">
+									<img src={OpenseaIcon} alt="OpenseaIcon" />
+								</a>
+							</li>
+							<li>
+								<a className="link" onClick={() => setShowMobileMenu(false)} href="https://twitter.com/">
+									<img src={TwitterIcon} alt="TwitterIcon" />
+								</a>
+							</li>
+							<li>
+								<a className="link" onClick={() => setShowMobileMenu(false)} href="https://discord.com/">
+									<img src={DiscordIcon} alt="DiscordIcon" />
+								</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			)}
+		</div>
+	)
 }
 
 export default Navbar
