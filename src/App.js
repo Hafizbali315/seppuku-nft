4f5561b98ee392cbb837db5c73cf4c0860486ce9
hafizbali315 <hafizbali315@gmail.com>
2022-08-02 09:24:42 +0500

    Lore Section Added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import Lore from './components/Lore'
 import Teams from './components/Teams'
 import Faqs from './components/Faqs'
 
+import FaqsRedDrops from './assets/images/FaqsRedDrops.png'
+import RoadmapRedDrops from './assets/images/RoadmapRedDrops.png'
+import VerticalSword from './assets/images/VerticalSword.png'
+
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Honor />
-			<Roadmap />
+
+			<div className="roadmap_graphics">
+				<Roadmap />
+
+				<img className="red_drops" src={RoadmapRedDrops} alt="RoadmapRedDrops" />
+				<img className="sword" src={VerticalSword} alt="VerticalSword" />
+			</div>
+
 			<Lore />
 			<Teams />
-			<Faqs />
+
+			<div className="faq_graphics">
+				<Faqs />
+
+				<img className="red_drops" src={FaqsRedDrops} alt="FaqsRedDrops" />
+			</div>
 		</div>
 	)
 }
--- src/components/Faqs.js
+++ src/components/Faqs.js
@@ -1,5 +1,36 @@
+import FaqSectionBg from '../assets/images/FaqsBg.png'
+
 const Faqs = () => {
-	return <div className="faqs">Faqs</div>
+	return (
+		<div className="faqs" id="faq">
+			<div className="content_container">
+				<h2>FAQS</h2>
+				<p>
+					<span>Q: Why a banner project?</span>
+					<br />
+					A: We want to be unique, not your every day project you see pop up. We believe this will be the highest quality banner project to
+					date and future chapters of the project will be the same.
+				</p>
+
+				<p>
+					<span>Q: Is the team based in Japan?</span>
+					<br />
+					A: The team is primarily based in the US, but some in other countries as well. We have a diverse background from many cultures
+					including Japanese, Native American, Latinx, American, and others.
+				</p>
+
+				<p>
+					<span>Q: What are the plans after the banner and pfp drops? </span>
+					<br />
+					A: Chapter 3 will be something unique to the NFT scene, we will share details as we get them finalized and can share.
+				</p>
+			</div>
+
+			<div className="bg">
+				<img src={FaqSectionBg} alt="faq" />
+			</div>
+		</div>
+	)
 }
 
 export default Faqs
